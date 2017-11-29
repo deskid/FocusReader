24ef2b1e3ef0b24ad8f3a119876986f9ba727f44
deskid <deskidzhou@gmail.com>
2017-11-29 16:07:36 +0800

    update: upgrade gradle build plugin to 3.0.1

--- app/src/main/java/com/github/deskid/focusreader/activity/SplashActivity.kt
+++ app/src/main/java/com/github/deskid/focusreader/activity/SplashActivity.kt
@@ -1,6 +1,5 @@
 package com.github.deskid.focusreader.activity
 
-import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.view.View
@@ -34,13 +33,8 @@ class SplashActivity : BaseActivity() {
                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 
-        splashViewModel.splashImage.observe(this, Observer {
-            web_imageview.setImageUrl(it?.data)
-            web_imageview.postDelayed({
-                startActivity<MainActivity>()
-                finish()
-            }, 200)
-        })
+        startActivity<MainActivity>()
+        finish()
     }
 
 }
