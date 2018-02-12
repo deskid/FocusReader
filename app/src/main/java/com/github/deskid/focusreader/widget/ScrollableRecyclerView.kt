7cde73048bef79482e668d59cb74c562131974d1
deskid <deskidzhou@gmail.com>
2018-02-12 18:33:12 +0800

    fix: recylerview load more

--- app/src/main/java/com/github/deskid/focusreader/widget/ScrollableRecyclerView.kt
+++ app/src/main/java/com/github/deskid/focusreader/widget/ScrollableRecyclerView.kt
@@ -1,17 +1,27 @@
 package com.github.deskid.focusreader.widget
 
 import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
 
 class ScrollableRecyclerView(context: Context?, attrs: AttributeSet?) : RecyclerView(context, attrs) {
 
     var loadMoreListener: () -> Unit = {}
+//
+//    override fun onScrollStateChanged(state: Int) {
+//        super.onScrollStateChanged(state)
+//        if (!canScrollVertically(1)) {
+//            loadMoreListener()
+//        }
+//    }
 
-    override fun onScrollStateChanged(state: Int) {
-        super.onScrollStateChanged(state)
-        if (!canScrollVertically(1)) {
-            loadMoreListener()
+    override fun onScrolled(dx: Int, dy: Int) {
+        super.onScrolled(dx, dy)
+        if (dy > 0 && !canScrollVertically(1)) {
+            if (layoutManager is LinearLayoutManager && (layoutManager as LinearLayoutManager).findLastVisibleItemPosition() == adapter.itemCount - 1) {
+                loadMoreListener()
+            }
         }
     }
 }
